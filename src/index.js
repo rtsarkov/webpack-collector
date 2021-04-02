08a34496dbed171968a57cbd23280f79ae56322b
rtsarkov <sarkofff1500@yandex.ru>
2021-04-02 22:29:42 +0300

    add configs

---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+import './style.css';
+import './style.scss'
+
+console.log('test');
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,104 @@
 const path = require('path');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-const isDev = true;
+const isDev = process.env.NODE_ENV === 'development';
 
-const babelOptions = preset => {
-  const opts = {
-    presets: [
-      '@babel/preset-env'
-    ],
-    plugins: [
-      '@babel/plugin-proposal-class-properties'
-    ]
-  }
-
-  if (preset) {
-    opts.presets.push(preset)
-  }
-
-  return opts
-}
-
-const jsLoaders = () => {
-  const loaders = [{
-    loader: 'babel-loader',
-    options: babelOptions()
-  }]
-
-  if (isDev) {
-    loaders.push('eslint-loader')
-  }
-
-  return loaders
-}
-
-module.export = {
+module.exports = {
 	context: path.resolve(__dirname, 'src'),
-	mode: 'developer',
-	entry: './index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'my-first-webpack.bundl.js'
+	mode: 'development',
+	devtool: 'source-map',
+	entry: {
+		main: './index.js'
 	},
+	resolve: {
+		extensions: ['.js', '.json', '.png'],
+		alias: {
+			'@module' : path.resolve(__dirname, 'src/moduls'),
+			'@': path.resolve(__dirname, 'src')
+		}
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'local/templates/main/assets'),
+	},
+	plugins: [
+		new MiniCssExtractPlugin(
+			{
+				filename: '[name].css',	
+			}
+		)
+	],
+	optimization: {
+	    minimize: true,
+	    minimizer: [
+	      new CssMinimizerPlugin({
+	        minify: async (data, inputMap) => {
+	          // eslint-disable-next-line global-require
+	          const CleanCSS = require('clean-css');
+
+	          const [[filename, input]] = Object.entries(data);
+	          const minifiedCss = await new CleanCSS({ sourceMap: true }).minify({
+	            [filename]: {
+	              styles: input,
+	              sourceMap: inputMap,
+	            },
+	          });
+
+	          return {
+	            code: minifiedCss.styles,
+	            map: minifiedCss.sourceMap.toJSON(),
+	            warnings: minifiedCss.warnings,
+	          };
+	        },
+	      }),
+	    ],
+	  },
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
-				exlude: /node-modules/,
-				use: jsLoaders()
+				exclude: /(node_modules|bower_components)/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env'],
+							plugins: ['@babel/plugin-transform-runtime']
+						}
+					}
+				]
+			},
+			{ 
+				test: /\.css$/, 
+				use: [
+					MiniCssExtractPlugin.loader,
+		        	{ 
+		        		loader: 'css-loader', 
+		        		options: { 
+		        			sourceMap: true 
+		        		} 
+		        	},
+				] 
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+		        	{ 
+		        		loader: 'css-loader', 
+		        		options: { 
+		        			sourceMap: true 
+		        		} 
+		        	},
+		        	{ 
+		        		loader: 'sass-loader', 
+		        		options: { 
+		        			sourceMap: true 
+		        		} 
+		        	},
+				]
 			}
 		]
 	}
